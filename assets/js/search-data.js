2e1e88827952ce28eb121f710b12c749ed184533
ajaymittur <ajaymittur28@gmail.com>
2026-07-12 07:56:07 +0000

    Deploying to gh-pages from @ ajaymittur/ajaymittur.github.io@f62831eb8dafa5f5808de081d6129d81da7b1855 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,8 +9,8 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
+  },{id: "nav-logs",
+          title: "logs",
           description: "",
           section: "Navigation",
           handler: () => {
